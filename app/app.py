ac725ce0b8a8e17b37a8f89d04811e4ddd2543c6
Unknown <finkol@gmail.com>
2018-04-02 11:31:34 +0200

    Add redis to docker and save json to redis

--- app/app.py
+++ app/app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+import os
+import redis
+from flask import Flask, request, Response
+import uuid
 
 app = Flask(__name__)
+
+r = redis.StrictRedis(host=os.environ['REDIS_HOST'], port=6379, db=0)
+
+
+@app.route("/json_dummy", methods=['PUT'])
+def put_json():
+    content = request.get_json()
+    uuid_str = str(uuid.uuid4())
+    r.set(uuid_str, content)
+    bla = r.get(uuid_str)
+    print(bla)
+    return "Received"
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!!!'
 
 
 if __name__ == "__main__":
